ab62df7b224dbf1064021006c096be503c6e58e5
Sascha Graeff <sascha@blackicegames.de>
2014-10-18 20:36:45 +0200

    Added Waypoint Connection Visualization.

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -4,4 +4,14 @@ using System.Collections.Generic;
 public class Waypoint : MonoBehaviour
 {
     public Waypoint next;
+
+    void OnDrawGizmos()
+    {
+        if(!next) return;
+
+        Gizmos.color = Color.yellow;
+
+        Gizmos.DrawRay(transform.position, Vector3.up);
+        Gizmos.DrawLine(transform.position + Vector3.up, next.transform.position);
+    }
 }
