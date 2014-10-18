794d8d0ae1244e1f856c02c170673adbda8acc3e
Sascha Graeff <sascha@blackicegames.de>
2014-10-18 16:04:27 +0200

    Made Waypoint.next public.

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -3,7 +3,5 @@ using System.Collections.Generic;
 
 public class Waypoint : MonoBehaviour
 {
-    [SerializeField]
-    private Waypoint _next;
-    public Waypoint next { get { return _next; } }
+    public Waypoint next;
 }
