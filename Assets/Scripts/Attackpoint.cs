8f9ab33f8fcf6f472716d758ece4a4addb8c7efe
trojan4 <j.deinert@gmx.net>
2014-10-18 17:33:24 +0200

    added attackpoint.cs

---
+++ Assets/Scripts/Attackpoint.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attackpoint : MonoBehaviour {
+
+	public int killAmount;
+	private RatSwarm rats;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Kill()
+	{
+		rats.KillAmount (killAmount);
+		}
+}
