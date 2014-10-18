601989323c972cd80d4d91356096d28fa1db68d0
Sascha Graeff <sascha@blackicegames.de>
2014-10-18 23:26:58 +0200

    Started over, moved Scripts.

---
+++ Assets/Scripts/First/BunchaRats.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BunchaRats : MonoBehaviour
+{
+    public BunchaRats next;
+    public float speed = 4;
+
+    public void MoveTo(Vector3 position)
+    {
+        if(next)
+        {
+            next.MoveTo(transform.position);
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, position, speed * Time.deltaTime);
+        CheckTriggerPoints();
+    }
+
+    private Vector3 lastGridPosition;
+    private void CheckTriggerPoints()
+    {
+        var gridPosition = transform.position;
+        gridPosition.x = Mathf.Round(gridPosition.x);
+        gridPosition.z = Mathf.Round(gridPosition.z);
+
+        if(gridPosition != lastGridPosition)
+        {
+            RaycastHit hit;
+            if(Physics.Raycast(gridPosition + Vector3.up * 10, Vector3.down, out hit, 10))
+            {
+                hit.collider.SendMessage("EnterSwarm", this, SendMessageOptions.DontRequireReceiver);
+            }
+        }
+
+        lastGridPosition = gridPosition;
+    }
+
+    public void AddBunch()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Sidescroller/SidescrollPlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SidescrollPlayer : MonoBehaviour
+{
+	void Awake()
+    {
+	
+	}
+
+	void Update()
+    {
+	
+	}
+}
