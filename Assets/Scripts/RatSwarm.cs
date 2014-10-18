b64454deb234bdf1727c0c3c848d8506892e969b
Sascha Graeff <sascha@blackicegames.de>
2014-10-18 17:37:12 +0200

    Rat awesomeness.

--- Assets/Scripts/RatSwarm.cs
+++ Assets/Scripts/RatSwarm.cs
@@ -5,11 +5,29 @@ public class RatSwarm : MonoBehaviour
 {
     [SerializeField]
     private Waypoint waypoint;
+    private TrailRenderer tr;
 
+    public int rats = 10000;
     public float speed = 5;
 
-	void Update()
+
+    void Awake()
     {
+        tr = GetComponentInChildren<TrailRenderer>();
+        tr.time = lineTime;
+    }
+
+	void FixedUpdate()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            KillAmount(1000);
+        }
+
+        tr.time = Mathf.Lerp(tr.time, lineTime, Time.deltaTime * 10);
+        tr.material.mainTextureScale = new Vector2(speed * tr.time, 1);
+        tr.material.mainTextureOffset = new Vector2(-tr.time * 10, 0);
+
         if(!waypoint) return;
 
         var dist = Vector3.Distance(transform.position, waypoint.transform.position);
@@ -25,6 +43,11 @@ public class RatSwarm : MonoBehaviour
 
 	public void KillAmount(int amount)
 	{
-
+        rats -= amount;
 	}
+
+    public float lineTime
+    {
+        get { return rats * (25f / 10000f / speed); }
+    }
 }
