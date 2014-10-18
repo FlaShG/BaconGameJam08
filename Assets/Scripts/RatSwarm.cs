1bf2e03f3da118bdb8644b1ab284a08058817616
Sascha Graeff <sascha@blackicegames.de>
2014-10-18 15:29:23 +0200

    Improved gitignore, added rat stuff.

---
+++ Assets/Scripts/RatSwarm.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RatSwarm : MonoBehaviour
+{
+	void Awake()
+    {
+	
+	}
+
+	void Update()
+    {
+	
+	}
+}
---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Waypoint : MonoBehaviour
+{
+    public static List<Waypoint> currentWaypoints;
+
+	void Awake()
+    {
+	
+	}
+
+	void Update()
+    {
+	
+	}
+}
