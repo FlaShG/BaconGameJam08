acbd76fd0b3bb171246ab4f262a00dc078d9e454
Sascha Graeff <sascha@blackicegames.de>
2014-10-19 00:40:37 +0200

    Initial KoksUndNutten concept.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float power = 20;
+
+	void FixedUpdate()
+    {
+        rigidbody2D.AddForce(new Vector3(Input.GetAxis("Horizontal") * power * Time.deltaTime * 50, 0, 0));
+
+        var pos = transform.position;
+
+        pos.y = 0;
+
+        transform.position = pos;
+	}
+}
