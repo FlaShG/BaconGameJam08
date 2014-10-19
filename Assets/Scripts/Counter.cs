11f8e5282aede824acf594ac74708861fcd5ff6d
trojan4 <j.deinert@gmx.net>
2014-10-19 20:23:04 +0200

    added counter animated skyline more hookers

---
+++ Assets/Scripts/Counter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Counter : MonoBehaviour {
+
+	public TextMesh text;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+	}
+
+	public void UpdateText(int count)
+	{
+		text.text = "$ " + count;
+	}
+	
+}
