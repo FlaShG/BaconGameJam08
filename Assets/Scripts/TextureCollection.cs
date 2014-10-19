1c3a0f39e3b51492b71442d563790a5cbf344484
AC-Suchti <frederikwille@arcor.de>
2014-10-19 16:26:25 +0200

    zoom

---
+++ Assets/Scripts/TextureCollection.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class TextureCollection : ScriptableObject
+{
+
+    public Sprite[] sprites;
+
+    public Sprite GetRandom()
+    {
+        return sprites[Random.Range(0, sprites.Length)];
+    }
+#if UNITY_EDITOR
+    [MenuItem("ScriptableObject/Create Sound Collection")]
+    static void CreateAsset()
+    {
+        ScriptableObject asset = ScriptableObject.CreateInstance(typeof(TextureCollection));
+        AssetDatabase.CreateAsset(asset, "Assets/New Sound Collection.asset");
+        EditorUtility.FocusProjectWindow();
+        Selection.activeObject = asset;
+    }
+#endif
+}
