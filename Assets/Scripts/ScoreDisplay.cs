d04547bc6b5b99edfa199f9eb40b6c94753768fa
Sascha Graeff <sascha@blackicegames.de>
2014-10-20 00:16:09 +0200

    more fixes

--- Assets/Scripts/ScoreDisplay.cs
+++ Assets/Scripts/ScoreDisplay.cs
@@ -7,5 +7,21 @@ public class ScoreDisplay : MonoBehaviour {
 
 	void Start () {
         text.text = "Score: $ " + Counter.cash;
-	}	
+	}
+    
+    	// Update is called once per frame
+	void Update () {
+#if (UNITY_ANDROID || UNITY_IPHONE) && !UNITY_EDITOR
+        if(Input.touchCount>0)
+        {
+            Application.LoadLevel("menu");
+        }
+#else
+                       if (Input.anyKey)
+        {
+            Application.LoadLevel("menu");
+        }
+#endif
+	    
+	}
 }
